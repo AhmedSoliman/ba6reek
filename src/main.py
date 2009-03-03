22333fed29e3e7379a20af45d21ff06cdcdaae1e
Ahmed Soliman Farghal <asoliman@soliman-thebe.localdomain>
2009-03-04 00:05:06 +0200

    some good modifications :)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
-import lettersReplacements
+#!/usr/bin/env python
+import sys
 import filters
 
 
 def main():
-    word = "3'aly 3layh"
+    if len(sys.argv) < 2:
+        print "Usage:%s <franko string>" % sys.argv[0]
+        sys.exit(1)
+    
+    word = sys.argv[1]
     normalizedWord = filters.normalize(word)
     print filters.generatePlainReplacement(normalizedWord)
     
